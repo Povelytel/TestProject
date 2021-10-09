93da4ccdd33fd6a16d2e982284c3df410ba2fcc9
Povelytel <sashamakaruk@gmail.com>
2021-10-09 21:41:50 +0300

    Add configuration file.

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const { time } = require("console");
-
-const express = require("express"),
-    bodyParser = require("body-parser"),
-    main = require('./app/routes/main'),
-    { Telegraf } = require('telegraf'),
+const   { Telegraf } = require('telegraf'),
     chatBot = require('./app/libs/chat_bot.js'),
-    app = express();
+    fs = require("fs");
 
-let bot = new chatBot({
-    token: '2090794937:AAHQSENzmPfbA6u7pRN0H8R02XKOQuzzyac'
+let fname = "./cfg.json",
+    configJS = fs.readFileSync(fname, 'utf8'),
+    cfg = JSON.parse(configJS);
+    bot = new chatBot({
+    token: cfg.token
 });
 
 //telegram bot check
 bot.check();
 
-
 /* api for test
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    main = require('./app/routes/main'),
+    app = express();
+
 const host = process.env.HOST || "127.0.0.1",
     port = process.env.PORT || "3030";
 
